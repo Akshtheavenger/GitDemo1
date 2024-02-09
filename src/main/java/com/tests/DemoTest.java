9a1e63650ecfb35725208c3aa00f0fdcbc4fbb0b
AkshayShadakshari3009 <AkshayShadakshari@dckap.com>
2024-02-09 21:39:56 +0530

    First Commit

---
+++ src/main/java/com/tests/DemoTest.java
@@ -0,0 +1,10 @@
+package com.tests;
+
+import static java.lang.System.*;
+
+public class DemoTest {
+
+    public static void main(String[] args) {
+        out.println("I am on Demo Test");
+    }
+}
